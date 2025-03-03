3bbcb8f62e6cca7681cf3f3b0891b74c7d5afd5c
Saga690 <master.ayush.2003@gmail.com>
2025-03-04 01:00:32 +0530

    linked gallery to drive

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -1,30 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
-
 function Gallery() {
-  const images = [
-    "https://media.istockphoto.com/id/485371557/photo/twilight-at-spirit-island.jpg?s=612x612&w=0&k=20&c=FSGliJ4EKFP70Yjpzso0HfRR4WwflC6GKfl4F3Hj7fk=",
-    "https://picsum.photos/200/300?image=1050",
-    "https://picsum.photos/300/300?image=206",
-    "https://picsum.photos/200/300?image=1050",
-    "https://picsum.photos/300/300?image=206",
-    "https://picsum.photos/200/300?image=1050",
-    "https://picsum.photos/300/300?image=206",
-    "https://picsum.photos/200/300?image=1050",
-    "https://picsum.photos/300/300?image=206",
-    "https://earthwatcher.photo.blog/wp-content/uploads/2019/07/grand-tetons-at-twilight-from-schwabachers-landing.jpg"
-  ];
+  const [images, setImages] = useState([]);
+  const folderId = process.env.REACT_APP_FOLDER_ID;
+  const apiKey = process.env.REACT_APP_API_KEY;
+
+
+  useEffect(() => {
+    fetch(
+      `https://www.googleapis.com/drive/v3/files?q='${folderId}'+in+parents+and+mimeType+contains+'image/'&key=${apiKey}&fields=files(id,name)`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        const imageUrls = data.files.map(
+          (file) => `https://lh3.googleusercontent.com/d/${file.id}=s1000`
+        );
+        // console.log(imageUrls);
+        setImages(imageUrls);
+        // console.log("images", images);
+      })
+      .catch((error) => console.error("Error fetching images:", error));
+  }, []);
+
   return (
     <div>
       <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
         <Masonry columnsCount={4} gutter="10px">
           {images.map((image, i) => (
-            <img
-              key={i}
-              src={image}
-              style={{ width: "100%", display: "block" }}
-            />
+            <img key={i} src={image} style={{ width: "100%", display: "block" }} />
           ))}
         </Masonry>
       </ResponsiveMasonry>
